87fa921196bb190deea532bb5f278efe75b9b3fd
kggold4 <68167734+kggold4@users.noreply.github.com>
2021-11-16 16:23:08 +0200

    ordering notebook and update gitignore

--- util_functions.py
+++ util_functions.py
@@ -1,7 +1,10 @@
 # util helping functions
+
+
 class util_functions:
+
+    @staticmethod
     def swap(arr, i , j):
-        print(arr)
-        r_t = arr[i]
+        temp = arr[i]
         arr[i] = arr[j]
-        arr[j] = r_t
+        arr[j] = temp
