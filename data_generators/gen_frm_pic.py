96644f33d5aba317a2a20ef1ce28a8f822b32b00
kggold4 <68167734+kggold4@users.noreply.github.com>
2021-11-16 16:13:54 +0200

    add train notebook and prepare data before split, create util_functions class

--- data_generators/gen_frm_pic.py
+++ data_generators/gen_frm_pic.py
@@ -1,18 +1,24 @@
 import numpy as np
 from PIL import Image as mg
 from matplotlib import pyplot as plt
-if __name__ == '__main__':
+IMAGE = 'elon.jpg'
 
-    img = plt.imread('elon.jpg')
-    img = np.asarray(img)  # first and second dimension are the matrix, third dimension is the channel
-    data = []
-    for _y in range(0, len(img) - 3, 3):
-        for _x in range(0, len(img[_y]) - 3, 3):
+class gen_frm_pic:
+    def generate():
 
-            mat = np.zeros((3, 3, 3), dtype=np.uint8)
-            for i in range(3):
-                for j in range(3):
-                    mat[i][j] = np.asarray(img[_y + i][_x + j])
-            data.append(mat)
+        img = plt.imread(IMAGE)
 
-    print(len(data))
+        # first and second dimension are the matrix, third dimension is the channel
+        img = np.asarray(img)
+        data = []
+        for _y in range(0, len(img) - 3, 3):
+        
+            for _x in range(0, len(img[_y]) - 3, 3):
+
+                mat = np.zeros((3, 3, 3), dtype=np.uint8)
+                for i in range(3):
+                    for j in range(3):
+                        mat[i][j] = np.asarray(img[_y + i][_x + j])
+                data.append(mat)
+
+        return data
---
+++ util_functions.py
@@ -0,0 +1,7 @@
+# util helping functions
+class util_functions:
+    def swap(arr, i , j):
+        print(arr)
+        r_t = arr[i]
+        arr[i] = arr[j]
+        arr[j] = r_t
