b5f6ab73e3509ec1648defb4016e7cb5e7dda61f
Gilad Shotland <giladsh123@gmail.com>
2021-11-21 15:08:26 +0200

    simple regression with 8 features

--- data_generators/gen_frm_pic.py
+++ data_generators/gen_frm_pic.py
@@ -11,14 +11,14 @@ class gen_frm_pic:
         # first and second dimension are the matrix, third dimension is the channel
         img = np.asarray(img)
         data = []
-        for _y in range(0, len(img) - 3, 3):
-        
-            for _x in range(0, len(img[_y]) - 3, 3):
+        for _y in range(1000, 1093, 3):
+
+            for _x in range(400, 500, 3):
 
                 mat = np.zeros((3, 3, 3), dtype=np.uint8)
                 for i in range(3):
                     for j in range(3):
                         mat[i][j] = np.asarray(img[_y + i][_x + j])
                 data.append(mat)
-
+        print(data)
         return data
---
+++ train.py
@@ -0,0 +1,6 @@
+from data_generators.gen_from_pic import *
+
+if __name__== '__main__':
+    data = generate()
+
+
